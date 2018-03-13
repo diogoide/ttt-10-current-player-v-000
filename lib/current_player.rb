de0e231ec8fda812a9b54dfea25b0eb490d18764
Diogo Ide <diogoide@gmail.com>
2018-03-13 23:12:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,10 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 counter = 0
 
 def turn_count(board)
+    if counter == "X" || "O" counter = counter + 1
     board.each do |counter|
     puts "Number of turns is #{counter}"
-    if counter == "X" || "O" counter += 1
+    
 end
 end
 end
